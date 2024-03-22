cc0fb742a35468f5bfc2379345cd410f53b2ef33
Pham Hung Anh <panh19032003@gmail.com>
2024-03-22 15:48:23 +0700

    online shopping

--- src/main/java/com/team3/onlineshopping/information/GoogleConfig.java
+++ src/main/java/com/team3/onlineshopping/information/GoogleConfig.java
@@ -9,10 +9,10 @@ package com.team3.onlineshopping.information;
  * @author PC
  */
 public class GoogleConfig {
-    	public static String GOOGLE_CLIENT_ID = "";
-	public static String GOOGLE_CLIENT_SECRET = "";
-	public static String GOOGLE_REDIRECT_URI = "";
-	public static String GOOGLE_LINK_GET_TOKEN = "";
-	public static String GOOGLE_LINK_GET_USER_INFO = "";
-	public static String GOOGLE_GRANT_TYPE = "";
+	public static String GOOGLE_CLIENT_ID = "286912111404-bm0reb1muj96f9n55sm1tkadi6gq1c3v.apps.googleusercontent.com";
+    	public static String GOOGLE_CLIENT_SECRET = "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";
+    	public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/onlineshopping/cus_googlelogin";
+    	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    	public static String GOOGLE_GRANT_TYPE = "authorization_code";
 }
